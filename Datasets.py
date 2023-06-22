588169d326046c26ef8d9a8f031389eba595f28a
Fran Soler <f.soler@umh.es>
2023-06-22 10:32:59 +0200

    Merged with dev branch

--- Datasets.py
+++ Datasets.py
@@ -7,7 +7,6 @@ import pandas as pd
 import open3d as o3d
 from tqdm import tqdm
 
-
 class PLYDatasetPlaneCount(Dataset):
     def __init__(self, root_dir = 'my_dataset_dir', features=None, labels_file='plane_count.csv', normalize=False):
         super().__init__()
@@ -57,6 +56,110 @@ class PLYDatasetPlaneCount(Dataset):
 
         return features, label, filename
 
+
+class PLYDataset_V2(Dataset):
+    def __init__(self,
+                 _mode = 'train', 
+                 _root_dir = 'my_dataset_dir',
+                 _coord_idx = None,
+                 _feat_idx = None,
+                 _label_idx = None,
+                 _normalize = False,
+                 _binary = False,
+                 _add_range = False, 
+                 _compute_weights = False):
+        
+        super().__init__()
+        self.mode = _mode
+        self.root_dir = _root_dir
+        self.cord_idx = _cord_idx if _cord_idx is not None else [0, 1, 2]
+        self.feat_idx = _feat_idx if _feat_idx is not None else [0, 1, 2]
+        self.label_idx = _label_idx if _label_idx is not None else [-1]
+        self.normalize = _normalize
+        self.binary = _binary
+        self.add_range = _add_range
+        self.compute_weights = _compute_weights
+        self.weights = []
+        self.coords = []
+        self.features = []
+        self.labels = []
+
+        self.dataset = []
+        for file in os.listdir(self.root_dir):
+            if file.endswith(".ply"):
+                self.dataset.append(file)
+
+        if self.compute_weights:
+            self.weights = []
+            # COMPUTE WEIGHTS FOR EACH LABEL IN THE WHOLE DATASET
+            print('-'*50)
+            print("COMPUTING LABEL WEIGHTS")
+            for file in tqdm(self.dataset):
+                # READ THE FILE
+                path_to_file = os.path.join(self.root_dir, file)
+                ply = PlyData.read(path_to_file)
+                data = ply["vertex"].data
+                data = np.array(list(map(list, data)))
+
+                # CONVERT TO BINARY LABELS
+                labels = data[:, self.label_idx].copy()
+
+                if self.binary:
+                    labels[labels > 0] = 1
+
+                labels = np.sort(labels, axis=None)
+                k_lbl, weights = np.unique(labels, return_counts=True)
+                # SI SOLO EXISTE UNA CLASE EN LA NUBE (SOLO SUELO)
+                if k_lbl.size < 2:
+                    if k_lbl[0] == 0:
+                        weights = np.array([1, 0])
+                    else:
+                        weights = np.array([0, 1])
+                else:
+                    weights = weights / len(labels)
+
+                if len(self.weights) == 0:
+                    self.weights = weights
+                else:
+                    self.weights = np.vstack((self.weights, weights))
+
+            self.weights = np.mean(self.weights, axis=0).astype(np.float32)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        file = self.dataset[index]
+        path_to_file = os.path.join(self.root_dir, file)
+        ply = PlyData.read(path_to_file)
+        data = ply["vertex"].data
+        # nm.memmap to np.ndarray
+        data = np.array(list(map(list, data)))
+
+        self.coords = data[:, self.cord_idx].copy()
+        self.features = data[:, self.feat_idx].copy()
+        self.labels = data[:, self.label_idx].copy()
+
+        if self.feat_idx[:3] == [0, 1, 2]:
+            if self.normalize:
+                xyz = self.coords.copy()
+                centroid = np.mean(xyz, axis=0)
+                xyz -= centroid
+                furthest_distance = np.max(np.sqrt(np.sum(abs(xyz) ** 2, axis=-1)))
+                xyz /= furthest_distance
+                self.features[:, [0, 1, 2]] = xyz
+
+        if self.add_range:
+            xyz = self.coords.copy()
+            range = np.sqrt(np.sum(abs(xyz) ** 2, axis=-1))
+            range = range[:, None]
+            self.features = np.hstack((self.features, range))
+
+        if self.binary:
+            self.labels[self.labels > 0] = 1
+
+
+        return self.coords, self.features, self.labels, self.dataset[index].split('.')[0]
 
 class PLYDataset(Dataset):
     def __init__(self, root_dir = 'my_dataset_dir', features=None, labels=None, normalize=False, binary = False, add_range_=False,compute_weights=False):
@@ -180,7 +283,7 @@ class MinkDataset(Dataset):
         self.voxel_size = _voxel_size
         self.normalize = _normalize
         self.binary = _binary
-        self.cords = []
+        self.coords = []
         self.features = []
         self.labels = []
 
@@ -200,66 +303,126 @@ class MinkDataset(Dataset):
         # nm.memmap to np.ndarray
         data = np.array(list(map(list, data)))
 
-        self.cords = data[:, self.cord_idx]
-        self.features = data[:, self.feat_idx]
-        self.labels = data[:, self.label_idx]
+        self.coords = data[:, self.cord_idx].copy()
+        self.features = data[:, self.feat_idx].copy()
+        self.labels = data[:, self.label_idx].copy()
+
+        if self.feat_idx[:3] == [0, 1, 2]:
+            if self.normalize:
+                # XYZ suposed to be 3 first features
+                xyz = self.coords.copy()
+                centroid = np.mean(xyz, axis=0)
+                xyz -= centroid
+                furthest_distance = np.max(np.sqrt(np.sum(abs(xyz) ** 2, axis=-1)))
+                xyz /= furthest_distance
+                self.features[:, [0, 1, 2]] = xyz
+
+        if self.add_range:
+            range = np.sqrt(np.sum(abs(self.coords) ** 2, axis=-1))
+            range = range[:, None]
+            self.features = np.hstack((self.features, range))
 
         if self.binary:
             self.labels[self.labels > 0] = 1
 
-        if self.normalize:
-            # XYZ suposed to be 3 first features
-            xyz = self.cords
-            centroid = np.mean(xyz, axis=0)
-            xyz -= centroid
-            furthest_distance = np.max(np.sqrt(np.sum(abs(xyz) ** 2, axis=-1)))
-            xyz /= furthest_distance
-            self.cords = xyz
-            self.features[:, [0, 1, 2]] = xyz
-
-        if self.add_range:
-            xyz = self.features[:, [0, 1, 2]]
-            D = np.sqrt(np.sum(abs(xyz) ** 2, axis=-1))
-            D = D[:, None]
-            self.features = np.hstack((self.features, D))
-
         if self.mode == 'test_no_labels':
-            return self.cords.astype(np.float32) / self.voxel_size, self.features.astype(np.float32)
+            return self.coords.astype(np.float32) / self.voxel_size, self.features.astype(np.float32)
         else:
-            return self.cords.astype(np.float32) / self.voxel_size, self.features.astype(np.float32), self.labels.astype(np.int32)
+            return self.coords.astype(np.float32) / self.voxel_size, self.features.astype(np.float32), self.labels.astype(np.int32)
 
 
-class VisMinkDataset(Dataset):
+class minkDataset(Dataset): # DEPRECATED
 
-    def __init__(self,
-                 _root_dir = 'my_dataset_dir',
-                 common_clouds_dir='',
-                 extend_clouds=[],
-                 _cord_idx=None,
-                 _feat_idx=None,
-                 _label_idx=None,
-                 _normalize=False,
-                 _binary=False,
-                 _add_range=False,
-                 _voxel_size=0.05):
-
+    def __init__(self, mode_='train', root_dir = 'my_dataset_dir', features=None, labels=None, normalize=False, binary = False, add_range_=False, voxel_size_=0.05):
         super().__init__()
-        self.root_dir = _root_dir
-        self.cord_idx = _cord_idx if _cord_idx is not None else [0, 1, 2]
-        self.feat_idx = _feat_idx if _feat_idx is not None else [0, 1, 2]
-        self.label_idx = _label_idx if _label_idx is not None else [-1]
-        self.add_range = _add_range
-        self.voxel_size = _voxel_size
-        self.normalize = _normalize
-        self.binary = _binary
-        self.cords = []
-        self.features = []
-        self.labels = []
+        self.root_dir = root_dir
+        self.add_range = add_range_
+        self.coords = []
+        self.voxel_size = voxel_size_
+        self.mode = mode_
+
+        if features is None:
+            self.features = [0, 1, 2]
+        else:
+            self.features = features
+
+        if labels is None:
+            self.labels = [-1]
+        else:
+            self.labels = labels
+
+        self.normalize = normalize
+        self.binary = binary
 
         self.dataset = []
         for file in os.listdir(self.root_dir):
             if file.endswith(".ply"):
                 self.dataset.append(file)
+
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+    def __getitem__(self, index):
+        file = self.dataset[index]
+        path_to_file = os.path.join(self.root_dir, file)
+        ply = PlyData.read(path_to_file)
+        data = ply["vertex"].data
+        # nm.memmap to np.ndarray
+        data = np.array(list(map(list, data)))
+
+        features = data[:, self.features].copy()
+        # self.coords = features[:, [0,1,2]]
+        self.coords = data[:, [0,1,2]].copy()
+
+        if self.normalize:
+            # XYZ suposed to be 3 first features
+            xyz = features[:, [0,1,2]]
+            centroid = np.mean(xyz, axis=0)
+            xyz -= centroid
+            furthest_distance = np.max(np.sqrt(np.sum(abs(xyz) ** 2, axis=-1)))
+            xyz /= furthest_distance
+            features[:, [0,1,2]] = xyz
+
+        if self.add_range:
+            xyz = features[:, [0,1,2]]
+            D = np.sqrt(np.sum(abs(xyz) ** 2, axis=-1))
+            D = D[:, None]
+            features = np.hstack((features, D))
+
+        if self.mode == 'test_no_labels':
+            return self.coords.astype(np.float32)/self.voxel_size, features.astype(np.float32)
+
+        else:
+            labels = data[:, self.labels]
+            if self.binary:
+                labels[labels > 0] = 1
+
+            return self.coords.astype(np.float32) / self.voxel_size, features.astype(np.float32), labels.astype(np.int32)
+
+
+class vis_minkDataset(Dataset): # DEPRECATED, USE MinkeDataset instead and append specific clouds to the dataset property
+
+    def __init__(self, root_dir = 'my_dataset_dir',  common_clouds_dir='', extend_clouds=[], features=None, labels=None, normalize=False, binary = False, add_range_=False, voxel_size_=0.05):
+        super().__init__()
+        self.root_dir = root_dir
+        self.add_range = add_range_
+        self.coords = []
+        self.voxel_size = voxel_size_
+
+        if features is None:
+            self.features = [0, 1, 2]
+        else:
+            self.features = features
+
+        if labels is None:
+            self.labels = [-1]
+        else:
+            self.labels = labels
+
+        self.normalize = normalize
+        self.binary = binary
 
         self.dataset = []
         for file in os.listdir(common_clouds_dir):
@@ -269,11 +432,13 @@ class VisMinkDataset(Dataset):
             for file in extend_clouds:
                 self.dataset.append(os.path.join(self.root_dir, file))
 
+
     def __len__(self):
         return len(self.dataset)
 
+
     def __getitem__(self, index):
-        path_to_file = os.path.abspath(self.dataset[index]) #type: os.path
+        path_to_file = os.path.abspath(self.dataset[index])
         ply = PlyData.read(path_to_file)
         data = ply["vertex"].data
         # nm.memmap to np.ndarray
@@ -301,10 +466,10 @@ class VisMinkDataset(Dataset):
         if self.binary:
             labels[labels > 0] = 1
 
+
         return self.coords.astype(np.float32) / self.voxel_size, features.astype(np.float32), labels.astype(np.int32)
 
-
-class vis_Test_Dataset(Dataset):
+class vis_Test_Dataset(Dataset): # DEPRECATED, USE PLYDataset instead and append specific clouds to the dataset property
     def __init__(self, root_dir = 'my_dataset_dir', common_clouds_dir='', extend_clouds=[], features=None, labels=None, normalize=False, binary = False, add_range_=False, compute_weights=False):
         super().__init__()
         self.root_dir = root_dir
@@ -369,7 +534,7 @@ class vis_Test_Dataset(Dataset):
         return len(self.dataset)
 
     def __getitem__(self, index):
-        path_to_file = os.path.abspath(self.dataset[index]) #type: os.path
+        path_to_file = os.path.abspath(self.dataset[index])
         # path_to_file = os.path.join(self.root_dir, file)
         ply = PlyData.read(path_to_file)
         data = ply["vertex"].data
@@ -421,26 +586,69 @@ class RandDataset(Dataset):
 
 if __name__ == '__main__':
 
-    ROOT_DIR = os.path.abspath('/home/arvc/Fran/data/datasets/ARVCTRUSS/train/ply_xyzlabelnormal')
+    ROOT_DIR = os.path.abspath('/media/arvc/data/datasets/ARVCTRUSS/train/ply_xyzlabelnormal')
 
-    dataset = MinkDataset(_mode='train',
-                          _root_dir=ROOT_DIR,
-                          _cord_idx=[0, 1, 2],
-                          _feat_idx=[0, 1, 2],
-                          _label_idx=[3],
-                          _normalize=True,
-                          _binary=True,
-                          _add_range=False,
-                          _voxel_size=0.1)
 
-    train_loader = torch.utils.data.DataLoader(dataset=dataset,
+    NORMALIZANDO = False
+    BINARY = True
+    ADD_RANGE = True
+
+
+    dataset_v1 = minkDataset(mode_='train',
+                             root_dir=ROOT_DIR,
+                             add_range_=ADD_RANGE,
+                             normalize=NORMALIZANDO,
+                             binary=BINARY,
+                             features=[0, 1, 2],
+                             labels=[3],
+                             voxel_size_=0.1)
+
+    dataset_v2 = MinkDataset(_mode='train',
+                            _root_dir=ROOT_DIR,
+                            _cord_idx=[0, 1, 2],
+                            _feat_idx=[0, 1, 2],
+                            _label_idx=[3],
+                            _normalize=NORMALIZANDO,
+                            _binary=BINARY,
+                            _add_range=ADD_RANGE,
+                            _voxel_size=0.1)
+
+
+    loader_v1 = torch.utils.data.DataLoader(dataset=dataset_v1,
                                                batch_size=1,
-                                               shuffle=True,
+                                               shuffle=False,
                                                num_workers=1,
                                                pin_memory=True,
                                                drop_last=True)
+    
+    loader_v2 = torch.utils.data.DataLoader(dataset=dataset_v2,
+                                               batch_size=1,
+                                               shuffle=False,
+                                               num_workers=1,
+                                               pin_memory=True,
+                                               drop_last=True)
+    
+    for i in range(5):
+        data_v1 = loader_v1.dataset[i]
+        data_v2 = loader_v2.dataset[i]
 
-    for i, (coord, feat, labels) in enumerate(train_loader):
-        print(labels)
+        # PATHS
+        # print(dataset_v1.dataset[i])
+        # print(dataset_v2.dataset[i])
+
+        # COORDS
+        # print(data_v1[0][0])
+        # print(data_v2[0][0])
+        
+        # FEATURES
+        print(data_v1[1][0])
+        print(data_v2[1][0])
+
+        # LABELS
+        # print(data_v1[2][0])
+        # print(data_v2[2][0])
+
+        print('-'*10)
+
 
 
